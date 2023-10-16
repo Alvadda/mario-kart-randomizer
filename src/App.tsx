e3da34494f4972ee5e2fcd9aeb50f234848e062d
Christian Pansch <christian.pansch@uxma.com>
2023-10-16 23:43:48 +0200

    init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Test } from '@/components/button'
+
+function App() {
+    return (
+        <div className="w-full h-11 bg-slate-500">
+            <Test />
+        </div>
+    )
+}
+
+export default App
