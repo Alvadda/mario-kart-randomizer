aab51da0a464965f5db14d9f1a15a22cb8ddb2e7
Alvadda <pansch.c@t-online.de>
2023-11-14 20:29:13 +0100

    fix hight

--- src/layout/mainContainer.tsx
+++ src/layout/mainContainer.tsx
@@ -6,7 +6,7 @@ type MainContainerProps = {
 
 export const MainContainer = ({ children }: MainContainerProps) => {
     return (
-        <div className="h-full bg-gradient-to-b from-blue-600 to-blue-900 bg-cover w-full grid grid-rows-[max-content_1fr] text-white font-poppins">
+        <div className="h-full bg-cover w-full grid grid-rows-[max-content_1fr] text-white font-poppins">
             <Header />
             <div className="px-4 py-6 sm:px-8 sm:py-12 w-full h-full">{children}</div>
         </div>
